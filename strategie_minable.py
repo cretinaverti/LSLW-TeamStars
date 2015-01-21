e660f02769fc30c0ec32d7ae844241bce9f5ca73
ArwenUndomiel <camille.foret@hotmail.fr>
2015-01-21 20:27:11 +0100

    Update strategie_minable.py

--- strategie_minable.py
+++ strategie_minable.py
@@ -1,20 +1,24 @@
 from classes import *
 from protocole import *
 
-"Réactualiser les données puis relancer cette fonction jusqu'à la fin du match"
 def strategie_minable(carte):
-    mes_planetes=carte.mes_planetes
-    lg=len(carte.mes_planetes())
+    state=None
+    mes_planetes=carte.mes_planetes(carte)
+    lg=len(mes_planetes)
     l=len(carte.liste_planetes)
 
-    while lg<l:
-        for p in mes_planetes:
-            
+    while lg<l and not(Game_Over(state)) and not(End_of_Game(state):
+        carte.liste_planetes=getTabPlanetes(state)
+        mes_planetes=carte.mes_planetes(carte)
+        lg=len(mes_planetes)
+
+        for planete in mes_planetes:
             i = 0
-            while i < len(carte.dict_distances[p.identifiant]) and carte.**get_PLanete_By**(carte.dict_distances[i]).proprietaire == carte.couleur:
+    
+            while i < len(carte.planete_voisines(planete.identifiant)) and carte.get_panete_by(carte.planete_voisines(planete.identifiant)[i][1]).proprietaire == carte.couleur:
                 i += 1
                 
-            if i != len(carte.dict_distances[p.identifiant]):
-                toOrderMsg(100, p.identifiant, p.liste_voisins[i])
-         
-                mes_planetes.append(voisin)
+                if i != len(carte.planete_voisines(planete.identifiant)):
+                    toOrderMsg(carte.id_joueur,100, planete.identifiant, carte.planete_voisines(p.identifiant)[i][1])
+        state=state()
+    
