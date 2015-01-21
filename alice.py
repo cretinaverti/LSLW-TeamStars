ebb6856ed73a7c7c60a3497663bc666cdaf6f252
ArwenUndomiel <camille.foret@hotmail.fr>
2015-01-21 20:31:30 +0100

    Essai avec strategie minable
    Voir si ca fonctionne...

--- alice.py
+++ alice.py
@@ -33,5 +33,24 @@ def watchdog(carte):
 
 def ia(carte):
     
-    while (not Game_Over()):        #mise en place des stratégies et du robot
-        pass
+   # while (not Game_Over()):        #mise en place des stratégies et du robot
+   #     pass
+    state=None
+    mes_planetes=carte.mes_planetes(carte)
+    lg=len(mes_planetes)
+    l=len(carte.liste_planetes)
+
+    while lg<l and not(Game_Over(state)) and not(End_of_Game(state):
+        carte.liste_planetes=getTabPlanetes(state)
+        mes_planetes=carte.mes_planetes(carte)
+        lg=len(mes_planetes)
+
+        for planete in mes_planetes:
+            i = 0
+    
+            while i < len(carte.planete_voisines(planete.identifiant)) and carte.get_panete_by(carte.planete_voisines(planete.identifiant)[i][1]).proprietaire == carte.couleur:
+                i += 1
+                
+                if i != len(carte.planete_voisines(planete.identifiant)):
+                    toOrderMsg(carte.id_joueur,100, planete.identifiant, carte.planete_voisines(p.identifiant)[i][1])
+        state=state()
